ac79c6b80402c3518e001cb076402d78f265f227
Richard Raya <rdxzv.dev@gmail.com>
2026-02-14 10:12:42 -0300

    Revert "sched/core: Make sched_setaffinity() no-op"
    This reverts commit 000b61ca43d21637c9f971c8e2922d41971425f4.
    
    Change-Id: I918e7bb2afac7c7849c92b4c175001f3c869f1a1
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5254,7 +5254,6 @@ out_put_task:
 	return retval;
 }
 
-#if 0
 static int get_user_cpu_mask(unsigned long __user *user_mask_ptr, unsigned len,
 			     struct cpumask *new_mask)
 {
@@ -5265,7 +5264,6 @@ static int get_user_cpu_mask(unsigned long __user *user_mask_ptr, unsigned len,
 
 	return copy_from_user(new_mask, user_mask_ptr, len) ? -EFAULT : 0;
 }
-#endif
 
 /**
  * sys_sched_setaffinity - set the CPU affinity of a process
@@ -5278,7 +5276,6 @@ static int get_user_cpu_mask(unsigned long __user *user_mask_ptr, unsigned len,
 SYSCALL_DEFINE3(sched_setaffinity, pid_t, pid, unsigned int, len,
 		unsigned long __user *, user_mask_ptr)
 {
-#if 0
 	cpumask_var_t new_mask;
 	int retval;
 
@@ -5290,9 +5287,6 @@ SYSCALL_DEFINE3(sched_setaffinity, pid_t, pid, unsigned int, len,
 		retval = sched_setaffinity(pid, new_mask);
 	free_cpumask_var(new_mask);
 	return retval;
-#else
-	return 0;
-#endif
 }
 
 long sched_getaffinity(pid_t pid, struct cpumask *mask)
